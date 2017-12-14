9271d983405194f8a34b447165f91b09c3ac33c9
haranadhagupta <gharanadhgupta@hotmail.com>
2017-12-14 08:32:36 +0530

    modified folder structure and file content

--- simplereport/markup_gen.py
+++ simplereport/markup_gen.py
@@ -4,14 +4,14 @@ import jinja2
 import json
 
 
-def prepare_markup():
+def get_markup(rep_templ_file, input_json_file):
     templateLoader = jinja2.FileSystemLoader( searchpath="./" )
     templateEnv = jinja2.Environment( loader=templateLoader )
-    template = templateEnv.get_template( "./report_base.tmpl" )
-    templateVars = json.load(open("input.json"))
-    outputText = template.render(templateVars)
-    return outputText
+    template = templateEnv.get_template( rep_templ_file )
+    json_data = json.load(open(input_json_file))
+    return template.render(json_data)
 
 
 if __name__ == '__main__':
-    print prepare_markup()
+    rep_templ_file, input_json_file = "./report_base.tmpl", "input.json"
+    print get_markup(rep_templ_file, input_json_file)
