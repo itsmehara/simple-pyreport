773631137bd23fa2965787dcc0459489a8d4c406
itsmehara <gharanadhgupta@hotmail.com>
2017-12-14 11:02:25 +0530

    changes done to save markup to file.

--- simplereport/markup_gen.py
+++ simplereport/markup_gen.py
@@ -12,6 +12,28 @@ def get_markup(rep_templ_file, input_json_file):
     return template.render(json_data)
 
 
+def save_markup(markup, target_file):
+    with open(target_file, "w+") as fp:
+        fp.write(markup)
+
+
+def main(template, input_json, output):
+    markup = get_markup(template, input_json)
+    save_markup( markup, output )
+
+
 if __name__ == '__main__':
-    rep_templ_file, input_json_file = "./report_base.tmpl", "input.json"
-    print get_markup(rep_templ_file, input_json_file)
+    try:
+        rep_templ_file, \
+        input_json_file, \
+        output_file = "report_base.tmpl", \
+                      "input.json",\
+                      "output.html"
+        # markup = get_markup(rep_templ_file, input_json_file)
+        # save_markup( markup, output_file )
+        main(rep_templ_file, input_json_file, output_file)
+    except Exception as e:
+        print("Failed")
+        raise e
+    else:
+        print("Success")
